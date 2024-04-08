44efe828b976ea61604ccb241a8840584b8179da
Chuprin Anton <workchuprin105@gmail.com>
2024-04-08 21:35:28 +0300

    add props + conspectus

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 class HeaderComponent extends React.Component {
 
     anotherMethod(){
@@ -13,11 +14,11 @@ class HeaderComponent extends React.Component {
     }
 }
 
-const component = React.createElement(HeaderComponent, {name: 'JOhn'});
+const component = React.createElement(HeaderComponent, {name: 'JOhn'}, 'TEXT--JOHN');
 
-const component2 = React.createElement(HeaderComponent, {name: 'TOHA'});
+const component2 = React.createElement(HeaderComponent, {name: 'TOHA'},'TEXT--TOHA');
 
-const parentElement = React.createElement('secion',{}, component, component2)
+const parentElement = React.createElement('section',{}, component, component2)
 
 
 
@@ -30,3 +31,41 @@ const parentElement = React.createElement('secion',{}, component, component2)
 const root = document.querySelector('#root')
 
 ReactDOM.render(parentElement,  root)
+/*
+1. Реакт - бібілотека для створення користувацьких інтерфейсів
+2. Ви можете використовувати як звичайний "ванільний" JS, так можете використовувати і Реакт. Це може відбуватись одночасно
+3. Пропси - це дані, які можна передавати в компоненти для того, щоб налаштовувати зовнішній вигляд компонент.
+4. Для того, щоб Реакт нам щосб відобразив, нам потрібно викликати метод ReactDOM.render().
+Він приймає 2 параметри:
+- елемент, який потрібно відрендерити
+- елемент, в який потрібно рендерити 
+5. Реакт оперує Реакт-елементами.
+Реакт-елемент - це об'єкт.
+Реакт-елемент набагато легший, ніж той самий елемент у нативному DOM'і.
+6. З цих Реакт-елементів як маленьких блоків складаються Реакт-компоненти.
+А з Реакт-компонент складаються сторінки, які будуть бачити наші користувачі.
+
+*/
+
+
+
+/*
+class divComponent extends React.Component {
+    render(){
+        const div = React.createElement('div',{title: 'ИДИ НАХУЙ'},`Hello ${this.props.name}`)
+
+        return div
+    }
+}
+
+const name1 = React.createElement(divComponent, {name: 'Zalypa'})
+
+const name2 = React.createElement(divComponent, {name: 'Pydaras'})
+
+const parent = React.createElement('section',{},name1,name2)
+
+const root = document.querySelector('#root')
+
+ReactDOM.render(parent,  root)
+
+*/
